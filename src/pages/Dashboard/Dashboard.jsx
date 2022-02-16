b2884042a56047958e329392d8cb3c8404b93446
rhkearns <ryankearns217@gmail.com>
2022-02-16 14:35:18 -0500

    build dashboard"

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { fetchEvents } from '../../redux/actions/eventActions';
 
 const Dashboard = () => {
-	
-  return (
+	const events = useSelector(state => state.allEvents.events);
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		dispatch(fetchEvents());
+	});
+
+	return (
 		<>
 			<h1>Dashboard</h1>
-      <button>Create New</button>
+			<button>Create New</button>
+			{events?.map((event) => (
+				<Link key={event._id} to={`/events/${event._id}`}>
+					
+				</Link>
+			))}
 		</>
 	);
 };

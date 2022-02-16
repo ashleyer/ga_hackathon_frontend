454852fd7e2584b962fd18a153e645101f190392
rhkearns <ryankearns217@gmail.com>
2022-02-16 13:39:56 -0500

    build createEvent

---
+++ src/pages/CreateEvent/CreateEvent.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router';
+import EventForm from './EventForm'
+import { addEvent } from '../../services/eventService';
+
+const CreateEvent = () => {
+  const navigate = useNavigate()
+  const [event, setEvent] = useState()
+  const [formData, setFormData] = useState({
+    eventName: '',
+    description: '',
+    startDate: '',
+    endDate: '',
+    location: '',
+  })
+  const [validForm, setValidForm] = useState(false)
+
+  const handleChange = (e) => {
+    setFormData({...formData, [e.target.name]: e.target.value})
+  }
+
+  const handleCreateEvent = async (e) => {
+    e.preventDefault()
+    try {
+      const newEvent = await addEvent(formData)
+      navigate(`/events/${newEvent._id}`)
+    } catch (error) {
+      throw error
+    }
+  }
+
+  return (
+    <>
+      <EventForm 
+        event={event}
+        setEvent={setEvent}
+        handleCreateEvent={handleCreateEvent}
+        handleChange={handleChange}
+        validForm={validForm}
+        setValidForm={setValidForm}
+        formData={formData}
+        setFormData={setFormData}
+      />
+    </>
+  )
+}
+
+export default CreateEvent

a274ac5267226f14f35465e81b578f16a68858fa
rhkearns <ryankearns217@gmail.com>
2022-02-16 11:52:52 -0500

    build redux store and event services

---
+++ src/services/eventService.js
@@ -0,0 +1,67 @@
+import * as tokenService from './tokenService'
+const BASE_URL = `${process.env.REACT_APP_BACKEND_SERVER_URL}/api/events/`
+
+export const getAllEvents = async () => {
+  try { 
+    const res = await fetch(`${BASE_URL}`, {
+      method: "GET",
+      headers: {
+        'content-type': 'application/json',
+        "Authorization": `Bearer ${tokenService.getToken()}`
+      },
+    })
+    const data = await res.json()
+    return data
+  } catch (error) {
+    throw error
+  }
+}
+
+export const addEvent = async (event) => {
+  try {
+    const res = await fetch(`${BASE_URL}`, {
+      method: "POST",
+      headers: {
+        'content-type': 'application/json',
+        'Authorization': `Bearer ${tokenService.getToken()}`
+      },
+      body: JSON.stringify(event)
+    })
+    const data = await res.json()
+    return data
+  } catch (error) {
+    throw error
+  }
+}
+
+export const getEventById = async (eventId) => {
+  try{
+    const res = await fetch(`${BASE_URL}${eventId}`, {
+      method: "GET",
+      headers: {
+        "Authorization": `Bearer ${tokenService.getToken()}`
+      },
+    })
+    const data = await res.json()
+    return data
+  } catch (error) {
+    throw error
+  }
+}
+
+export const updateEvent = async (eventId, formData) => {
+  try {
+    const res = await fetch(`${BASE_URL}${eventId}`, {
+      method: "PUT",
+      headers: {
+        'content-type': 'application/json',
+        'Authorization': 'Bearer ' + tokenService.getToken()
+      },
+      body: JSON.stringify(formData)
+    })
+    const data = await res.json()
+    return data
+  } catch (error) {
+    throw error
+  }
+}

230000b0f452d2b4d61ece5ffa2aa18b9bdf91e8
rhkearns <ryankearns217@gmail.com>
2022-02-16 16:09:47 -0500

    create dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { deleteEvent } from '../../services/eventService';
+import fetchEvents from '../../redux/actions/eventActions';
+import { Link } from 'react-router-dom';
+import EventCard from '../../components/EventCard/EventCard';
 
 const Dashboard = () => {
-	
-	const handleDeleteEvent = async (eventId) => {
-		try {
-			await deleteEvent(eventId)
-			
-		} catch (error) {
-			throw error
-		}
-	}
+	const events = useSelector(state => state.allEvents.events);
+	const dispatch = useDispatch();
 
-  return (
+	useEffect(() => {
+		dispatch(fetchEvents());
+	});
+
+	const handleDeleteEvent = async eventId => {
+		try {
+			await deleteEvent(eventId);
+		} catch (error) {
+			throw error;
+		}
+	};
+
+	return (
 		<>
 			<h1>Dashboard</h1>
-      <button>Create New</button>
+			<button>Create New</button>
+			{events?.map(event => (
+				<Link key={event._id} to={`/events/${event._id}`}>
+					<EventCard event={event} handleDeleteEvent={handleDeleteEvent} />
+				</Link>
+			))}
 		</>
 	);
 };

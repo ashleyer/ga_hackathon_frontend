a9bb0d8a8d810b535af294b864c193d971e22615
rhkearns <ryankearns217@gmail.com>
2022-02-16 21:09:16 -0500

    details displaying

--- src/pages/EventDetails/EventDetails.jsx
+++ src/pages/EventDetails/EventDetails.jsx
@@ -1,42 +1,57 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import moment from 'moment';
 import * as eventService from '../../services/eventService';
-import { fetchSingleEvent, fetchEvents } from '../../redux/actions/eventActions';
-import { useSelector, useDispatch } from 'react-redux';
+// import { fetchSingleEvent, fetchEvents } from '../../redux/actions/eventActions';
+// import { useSelector, useDispatch } from 'react-redux';
 
 const EventDetails = () => {
 	const { id } = useParams();
-  const [event, setEvent] = useState();
-  const events = useSelector((state) => state.allEvents.events.events)
-  const dispatch = useDispatch()
-
-  const findEvent = (id) => {
-    const event = events.find(event => event._id === id)
-    // console.log('event', event);
-    setEvent(event)
-  }
+  const [event, setEvent] = useState({});
 
 	useEffect(() => {
-		dispatch(fetchSingleEvent(id));
-	}, []);
+		const fetchEvent = async () => {
+			try {
+        const eventData = await eventService.getEventById(id);
+        console.log('eventdata', eventData);
+				setEvent(eventData.event);
+			} catch (error) {
+				throw error;
+			}
+		};
+		fetchEvent();
+	}, [id]);
 
-  console.log('events', events);
-  console.log('event', event)
+	console.log('event', event);
 
 	return (
 		<>
 			<h1>Event Details</h1>
-			{/* <h1>{event.event.eventName}</h1>
-			<h3>{event.event.description}</h3>
-			<h3>{moment(event.event.startDate).utc().format('MM/DD/YYYY')}</h3>
+			<h1>{event.eventName}</h1>
+			<h3>{event.description}</h3>
+			<h3>{moment(event.startDate).utc().format('MM/DD/YYYY')}</h3>
 			{event.endDate && (
-				<h3>{moment(event.event.endDate).utc().format('MM/DD/YYYY')}</h3>
+				<h3>{moment(event.endDate).utc().format('MM/DD/YYYY')}</h3>
 			)}
-			<h3>{event.event.location}</h3> */}
+			<h3>{event.location}</h3>
 		</>
 	);
 };
 
 export default EventDetails;
+
+// const events = useSelector((state) => state.allEvents.events.events)
+// const dispatch = useDispatch()
+// const findEvent = (id) => {
+//   const event = events.find(event => event._id === id)
+//   // console.log('event', event);
+//   setEvent(event)
+// }
+
+// useEffect(() => {
+// 	dispatch(fetchEvents());
+//   findEvent(id)
+// }, []);
+
+// console.log('events', events);

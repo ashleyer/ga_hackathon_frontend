3610bce8cbda36b91ee191e1e8348157e4ea0465
rhkearns <ryankearns217@gmail.com>
2022-02-16 21:48:19 -0500

    add attendee form

--- src/components/AttendeeList/AddAttendee.jsx
+++ src/components/AttendeeList/AddAttendee.jsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const AddAttendee = () => {
-  return (
-    <h1>Add Attendee</h1>
-  )
-}
+const AddAttendee = props => {
+	const [formData, setFormData] = useState({
+		attendeeName: '',
+		email: '',
+	});
 
-export default AddAttendee
+	const handleChange = e => {
+		setFormData({ ...formData, [e.target.name]: e.target.value });
+	};
+
+	const handleSubmit = e => {
+		e.preventDefault();
+		props.handleAddAttendee(formData);
+		props.setToggleNew(false);
+	};
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<label>Attendee Name: </label>
+			<input
+				type="text"
+				autoComplete="off"
+				name="AttendeeName"
+				placeholder="Name"
+				onChange={handleChange}
+			/>
+			<label>Email: </label>
+			<input
+				type="text"
+				autoComplete="off"
+				name="email"
+				placeholder="Email Address"
+				onChange={handleChange}
+			/>
+			<button type="submit">Add Attendee</button>
+		</form>
+	);
+};
+
+export default AddAttendee;

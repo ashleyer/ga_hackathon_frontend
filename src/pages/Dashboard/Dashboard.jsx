17443c4cf6804bbd2d72dce550aeeb2bf734a5ea
rhkearns <ryankearns217@gmail.com>
2022-02-17 11:15:02 -0500

    new button clicks

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteEvent } from '../../services/eventService';
 import { fetchEvents } from '../../redux/actions/eventActions';
@@ -8,6 +9,7 @@ import EventCard from '../../components/EventCard/EventCard';
 const Dashboard = () => {
 	const events = useSelector(state => state.allEvents.events.events);
 	const dispatch = useDispatch();
+	const navigate = useNavigate()
 	console.log('events', events);
 	
 	useEffect(() => {
@@ -25,7 +27,7 @@ const Dashboard = () => {
 	return (
 		<>
 			<h1>Dashboard</h1>
-			<button>Create New</button>
+			<button onClick={() => navigate('/newevent')}>Create New</button>
 			{events?.map(event => (
 				<Link key={event._id} to={`/events/${event._id}`}>
 					<EventCard event={event} handleDeleteEvent={handleDeleteEvent} />
